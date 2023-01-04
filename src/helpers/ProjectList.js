baf819a3584cad572b140af082e65887757d582e
Adlai <adlaibridsonboyczuk@gmail.com>
2023-01-04 18:38:16 -0500

    Edited ProjectList

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -2,6 +2,8 @@ import ForestRumble from '../assets/ForestRumble.JPG';
 import LogicProject from '../assets/LogicProjectimage.jpg';
 import JAImage from '../assets/ToteworthyImage.jpg';
 import qbnbimg from '../assets/qbnbss.jpg';
+import AdChat from '../assets/AdChatimg.jpg';
+import myLibrary from '../assets/download.png';
 
 export const ProjectList = [
     {
@@ -15,9 +17,14 @@ export const ProjectList = [
         skills: "C#, Unity, HTML"
     },
     {
-        name: "Herring - Isometric RPG game",
-        image: ForestRumble,
-        skills: "C#, Unity 2D"
+        name: "AdChat - basic messaging app / Discord clone",
+        image: AdChat,
+        skills: "NodeJS, ReactJS, ExpressJS, Stream.io"
+    },
+    {
+        name: "myLibrary - Personal Book manager",
+        image: myLibrary,
+        skills: "C++, MySQL, Sqlite3"
     },
     {
         name: "Stalemate - Eight Queen's Logic modelling",
@@ -28,5 +35,10 @@ export const ProjectList = [
         name: "Toteworthy - merchanise company website",
         image: JAImage,
         skills: "JavaScript, ReactJS, HTML, CSS"
+    },
+    {
+        name: "Herring - Isometric RPG game",
+        image: ForestRumble,
+        skills: "C#, Unity 2D"
     },
 ];
